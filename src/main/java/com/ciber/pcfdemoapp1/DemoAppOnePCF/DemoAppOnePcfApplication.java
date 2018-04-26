a21cf20e3fe59a83b735562d43e2c97d9dac0457
Harish Sekar <hasekar@ciber.com>
2018-04-26 09:22:32 -0400

    Initial Starter Spring Boot

---
+++ src/main/java/com/ciber/pcfdemoapp1/DemoAppOnePCF/DemoAppOnePcfApplication.java
@@ -0,0 +1,12 @@
+package com.ciber.pcfdemoapp1.DemoAppOnePCF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppOnePcfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppOnePcfApplication.class, args);
+	}
+}
